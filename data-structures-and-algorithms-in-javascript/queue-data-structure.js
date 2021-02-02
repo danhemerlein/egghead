56ddafd7d5fcff1f9fa76f5ea7388048f96e864b
Dan Hemerlein <danhemerlein@gmail.com>
2021-02-01 21:05:35 -0500

    queue data structure

---
+++ data-structures-and-algorithms-in-javascript/queue-data-structure.js
@@ -0,0 +1,48 @@
+// Queues
+// first in first out
+
+function createQueue() {
+  const queue = [];
+  return {
+    // add or enqueue
+    enqueue(item) {
+      // unshift adds to the beginning of an array
+      queue.unshift(item);
+    },
+
+    // remove or dequeue
+    dequeue(item) {
+      // pop removes the last item of an array
+      return queue.pop(item)
+    },
+    // peek
+    peek() {
+      return queue[queue.length - 1];
+    },
+    // length - need to use a getter function here
+    get length() {
+      return queue.length;
+    },
+    // isEmpty
+    isEmpty() {
+      return queue.length === 0;
+    }
+  }
+}
+
+const q = createQueue();
+console.log(q.isEmpty());
+
+q.enqueue('make an egghead lesson');
+q.enqueue('help others learn');
+q.enqueue('be happy');
+
+// ['be happy', 'help others learn', 'make an egghead lesson']
+
+q.dequeue();
+
+// ['be happy', 'help others learn']
+
+console.log(q.peek());
+
+console.log(q.isEmpty());
