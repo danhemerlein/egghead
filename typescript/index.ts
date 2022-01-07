14dccd84f07b198d1e2335e14a66a218c2420b95
Dan Hemerlein <danhemerlein@gmail.com>
2022-01-06 22:31:05 -0500

    complete

--- typescript/index.ts
+++ typescript/index.ts
@@ -1,4 +1,5 @@
 console.log('hello from typescript');
+import _ from "lodash";
 
 type Salutation = {greeting:string, name?:string}
 
@@ -14,3 +15,30 @@ const danMessage:string = greet({greeting: "Howdy", name: "Dan"});
 console.log(message)
 console.log(danMessage)
 
+const input = document.getElementById('text-input') as HTMLInputElement;
+
+input.autofocus = true;
+
+input.addEventListener("input", event => {
+  const i = event.currentTarget as HTMLInputElement
+  console.log(i.value);
+});
+
+
+function fill<T>(array:any[], value:T):T {
+  return array.map(() => value)
+}
+
+const result = fill([1,2,3], "a");
+
+console.log(result);
+
+result.map(x => x.toUpperCase());
+
+console.log(result);
+
+const resultTwo = fill(['a', 'b', 'c'], 4);
+
+console.log(resultTwo);
+
+console.log(_.uniq([1,1,2]));
